962cb8b69a518bb78d564e2959e5b1594c172d64
Jared Hightower <Jared.jhightower@gmail.com>
2020-04-26 01:25:39 -0500

    Updates

---
+++ precache-manifest.4492125c0a91e5ef5ef5c0b0caa145e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "016b60025349281da82794abac38cff9",
+    "url": "/monsters-rolodex-2/index.html"
+  },
+  {
+    "revision": "c258ceac71dcf793b5d5",
+    "url": "/monsters-rolodex-2/static/css/main.514f8ea4.chunk.css"
+  },
+  {
+    "revision": "78dd2bc8a741db6828c1",
+    "url": "/monsters-rolodex-2/static/js/2.dc0b79fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex-2/static/js/2.dc0b79fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c258ceac71dcf793b5d5",
+    "url": "/monsters-rolodex-2/static/js/main.a1d27d7c.chunk.js"
+  },
+  {
+    "revision": "aec39d123398253bbe34",
+    "url": "/monsters-rolodex-2/static/js/runtime-main.d776b490.js"
+  }
+]);
